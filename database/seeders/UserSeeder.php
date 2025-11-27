ca64c345e2f8c5f764847c37c48889010db32e78
ViniciusAndreGoncalves <vinicius.a234@protonmail.com>
2025-11-27 00:41:10 -0300

    wip: Criando usuário teste com seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::create([
+            'name' => 'user',
+            'email' => 'user@teste',
+            'password' => bcrypt('12345678'),
+        ]);
+    }
+}
