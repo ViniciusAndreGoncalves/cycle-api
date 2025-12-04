1bdd6f3e8ad54c324396d2acca5da7489a2307fa
ViniciusAndreGoncalves <vinicius.a234@protonmail.com>
2025-12-04 00:38:55 -0300

    Fix: correção nos parâmetros da Model

--- app/Models/Movimentacao.php
+++ app/Models/Movimentacao.php
@@ -14,6 +14,7 @@ class Movimentacao extends Model
     protected $fillable = [
         'carteira_id',
         'ativo_id',
+        'user_id',
         'tipo',
         'quantidade',
         'preco_unitario',
